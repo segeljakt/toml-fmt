22ab50f4d64831cffd28777870526cc97e430474
Pierre Fenoll <pierrefenoll@gmail.com>
2022-08-02 12:04:26 +0200

    lock and update deps + switch to edition 2021
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use std::io::{self, Read, Write};
 use std::error::Error;
-fn main() -> Result<(), Box<Error>> {
+use std::io::{stdin, stdout, Read, Write};
+use toml::de::from_str;
+use toml::ser::to_string_pretty;
+use toml::value::Value;
+
+fn main() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input)?;
-    let value: toml::value::Value = toml::de::from_str(&input)?;
-    let output = toml::ser::to_string_pretty(&value)?;
-    io::stdout().write_fmt(format_args!("{}", output))?;
+    stdin().read_to_string(&mut input)?;
+
+    let value: Value = from_str(&input)?;
+
+    let output = to_string_pretty(&value)?;
+    stdout().write_fmt(format_args!("{output}"))?;
+
     Ok(())
 }
